6114d9f9150865a62d9b6437722ffa8c8efc411e
Maraym-777 <maryam_abbasi88@yahoo.com>
2026-08-11 04:04:54 +0330

    style component with tailwind

--- src/components/InputBtn.jsx
+++ src/components/InputBtn.jsx
@@ -5,11 +5,13 @@ import { useState } from "react"
 const InputBtn = ({title}) => {
     const[inputValue,setValue]=useState("");
   return (
-    <div>
-        <h1>{title}</h1>
-        <div>
-            <input type="text" placeholder="write sth" onChange={(e)=>setValue(e.target.value)} />
-            <button onClick={()=>console.log(inputValue)}>click</button>
+    <div className="border border-pink-700 mt-6 p-2 rounded-2xl mx-2" >
+        <h1 className="mb-40 font-semibold text-[30px] text-pink-600">{title}</h1>
+        <div className="flex flex-col gap-3">
+            <input type="text" placeholder="write sth" onChange={(e)=>setValue(e.target.value)} 
+            className="border-pink-400 border focus:outline-none rounded-md p-1"/>
+            <button onClick={()=>console.log(inputValue)}
+              className="bg-pink-500 rounded-2xl text-amber-50 px-3 py-1 hover:bg-pink-400">click</button>
         </div>
     </div>
   )
--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -2,15 +2,15 @@
 
 const ProductsList = ({items,title}) => {
   return (
-    <div>
-        <h1>{title}</h1>
-        <div>
+    <div className="border border-pink-700 mt-6 p-2 rounded-2xl mx-2 ">
+        <h1 className="mb-40 font-semibold text-[30px] text-pink-600">{title}</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
             {
-              items.map((item)=>(
-                 <div>
-                  <p>{item.name}</p>
-                  <p>{item.price}</p>
-                  <p>{item.description}</p>
+              items.map((item,index)=>(
+                 <div className="border border-pink-700 rounded-2xl p-2 hover:bg-gray-300 " key={index}>
+                  <p className="text-emerald-400 text-2xl font-bold">{item.name}</p>
+                  <p className="text-fuchsia-900">{item.price}</p>
+                  <p className="text-amber-500 ">{item.description}</p>
                  </div>
               ))
             }
